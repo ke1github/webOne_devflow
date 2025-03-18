ecf7f199afa73cfa50150190cb8862745c622359
Kosi Engineers <kosiengineers@gmail.com>
2025-03-18 08:04:01 +0530

    eslint & prettier setup 1.1

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-black text-white ">Hfhf dsdggef sdggg  ome Page</h1>
+      <h1 className="text-3xl font-black text-white "> Welcome to the nexjs app</h1>
     </div>
   );
 }
