18805586b5e65e7cd59521caed8609cb3580f681
Kosi Engineers <kosiengineers@gmail.com>
2025-03-18 07:47:08 +0530

    Eslint & Prettier Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import React from 'react';
+
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold">Home</h1>
+      <h1 className="text-3xl font-black text-white ">Hfhf dsdggef sdggg  ome Page</h1>
     </div>
   );
 }
