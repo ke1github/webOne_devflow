91396757d7f418cae72de8ddbb5de02a712237a8
Kosi Engineers <kosiengineers@gmail.com>
2025-03-18 08:27:16 +0530

    Tailwind CSS Setup

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-black text-white "> Welcome to the nexjs app</h1>
+      <h1 className="text-3xl font-black bg-primary-500 text-primary-100 "> Welcome to the nexjs app</h1>
     </div>
   );
 }
